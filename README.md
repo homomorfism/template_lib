d779eda993522e5660cc71da5116189e85720d39
Your Name <you@example.com>
2020-11-09 11:58:36 +0300

    Started working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Template project electronic library
+
+https://www.styleshout.com/templates/preview/Abstract_2_0_0/index.html
--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from static.home.views import index_view
+from home.views import index_view
 
 urlpatterns = [
     path('', index_view, name='home')
